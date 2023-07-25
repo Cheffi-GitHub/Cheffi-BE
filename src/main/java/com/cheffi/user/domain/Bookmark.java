ffe59816a06ab2eecc24442e8e4d24569e4a0da8
lsh <seulee0862@gmail.com>
2023-07-25 22:59:31 +0900

    Feat: add entity Bookmark

---
+++ src/main/java/com/cheffi/user/domain/Bookmark.java
@@ -0,0 +1,29 @@
+package com.cheffi.user.domain;
+
+import com.cheffi.avatar.domain.Avatar;
+import com.cheffi.review.Review;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Bookmark {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @JoinColumn(name = "avatar_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Avatar avatar;
+
+    @NotNull
+    @JoinColumn(name = "review_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Review review;
+}
