737ed6ca29651d197bd2f3d6f4b81348dcc9b44e
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-07-25 15:48:12 +0900

    Feat: add `Notification`

---
+++ src/main/java/com/cheffi/avatar/domain/Notification.java
@@ -0,0 +1,38 @@
+package com.cheffi.avatar.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Notification {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	private String content;
+
+	private boolean checked;
+
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "avatar_id")
+	private Avatar avatar;
+
+	public Notification(String content, Avatar avatar) {
+		this.content = content;
+		this.avatar = avatar;
+		this.checked = false;
+	}
+}
