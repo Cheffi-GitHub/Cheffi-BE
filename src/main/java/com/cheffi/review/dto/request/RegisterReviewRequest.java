1516f955b963bb5a942937ed6b1786949a96515b
lsh <seulee0862@gmail.com>
2023-08-03 22:20:22 +0900

    Refactor: optimize import:

--- src/main/java/com/cheffi/review/dto/request/RegisterReviewRequest.java
+++ src/main/java/com/cheffi/review/dto/request/RegisterReviewRequest.java
@@ -7,11 +7,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 
 public record RegisterReviewRequest (
--- src/main/java/com/cheffi/review/dto/request/SearchReviewRequest.java
+++ src/main/java/com/cheffi/review/dto/request/SearchReviewRequest.java
@@ -2,7 +2,6 @@ package com.cheffi.review.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
 
 public record SearchReviewRequest(
     @Schema(description = "Review 식별자", example = "1")
