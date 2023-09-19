8f60a8ebfd876fb392c02eade88033661ded128c
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-09-19 15:01:47 +0900

    Refactor: change `Food` to `Menu`

--- src/main/java/com/cheffi/review/dto/MenuDto.java
+++ src/main/java/com/cheffi/review/dto/MenuDto.java
@@ -1,18 +1,12 @@
 package com.cheffi.review.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Getter;
 
-@Getter
-public class FoodDto {
-
+public record MenuDto(
 	@Schema(description = "메뉴 이름", example = "곱창 전골")
-	private final String name;
+	String name,
 	@Schema(description = "메뉴 가격", example = "80000")
-	private final int price;
-
-	public FoodDto(String name, int price) {
-		this.name = name;
-		this.price = price;
-	}
+	int price,
+	@Schema(description = "메뉴 설명, 입력하지 않아도 됩니다.")
+	String description) {
 }
--- src/main/java/com/cheffi/review/dto/request/WriteReviewRequest.java
+++ src/main/java/com/cheffi/review/dto/request/WriteReviewRequest.java
@@ -1,8 +1,10 @@
 package com.cheffi.review.dto.request;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import com.cheffi.review.dto.FoodDto;
+import com.cheffi.review.dto.MenuDto;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
@@ -12,17 +14,27 @@ import jakarta.validation.constraints.NotBlank;
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record WriteReviewRequest(
 	@NotBlank
-	@Schema(description = "식당 이름")
-	String restaurantName,
-	@NotBlank
-	@Schema(description = "식당 법정주소")
-	int addressCode,
+	@Schema(description = "식당 식별자")
+	int restaurantId,
 	@NotBlank
 	@Schema(description = "리뷰 제목")
 	String title,
 	@NotBlank
 	@Schema(description = "리뷰 내용")
 	String text,
+	List<MenuDto> menus,
+	@JsonProperty(value = "food_tags")
+	@Schema(description = "작성할 리뷰의 음식 태그의 식별자 리스트 ", example = "[0, 1, 2]")
+	List<Long> foodTags,
+	@JsonProperty(value = "taste_tags")
+	@Schema(description = "작성할 취향 태그의 식별자 리스트 ", example = "[3, 4]")
+	List<Long> tasteTags) {
 
-	List<FoodDto> menu) {
+	public List<Long> combinedTagList() {
+		List<Long> combined = new ArrayList<>();
+		combined.addAll(foodTags);
+		combined.addAll(tasteTags);
+		return combined;
+	}
+
 }
