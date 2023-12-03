b32352a99d207b5ace7e8e50924e00f32d193f73
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-12-03 16:31:11 +0900

    Feat: add `POST /bookmarks` and `DELETE /bookmarks`

---
+++ src/main/java/com/cheffi/review/controller/BookmarkController.java
@@ -0,0 +1,57 @@
+package com.cheffi.review.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cheffi.common.response.ApiResponse;
+import com.cheffi.oauth.model.UserPrincipal;
+import com.cheffi.review.dto.request.DeleteBookmarkRequest;
+import com.cheffi.review.dto.request.PostBookmarkRequest;
+import com.cheffi.review.service.BookmarkService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@Tag(name = "Bookmark")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/bookmarks")
+public class BookmarkController {
+
+	private final BookmarkService bookmarkService;
+
+	@Tag(name = "Bookmark")
+	@Operation(summary = "북마크 추가 API - 인증 필수",
+		description = "인증 필수, 북마크 추가",
+		security = {@SecurityRequirement(name = "session-token")})
+	@PreAuthorize("hasRole('USER')")
+	@PostMapping
+	public ApiResponse<Void> addBookmark(
+		@RequestBody @Valid PostBookmarkRequest request,
+		@AuthenticationPrincipal UserPrincipal principal) {
+		bookmarkService.addBookmark(principal.getAvatarId(), request.getReviewId());
+		return ApiResponse.success();
+	}
+
+	@Tag(name = "Bookmark")
+	@Operation(summary = "북마크 취소 API - 인증 필수",
+		description = "인증 필수, 북마크 취소",
+		security = {@SecurityRequirement(name = "session-token")})
+	@PreAuthorize("hasRole('USER')")
+	@DeleteMapping
+	public ApiResponse<Void> cancelBookmark(
+		@RequestBody @Valid DeleteBookmarkRequest request,
+		@AuthenticationPrincipal UserPrincipal principal) {
+		bookmarkService.cancelBookmark(principal.getAvatarId(), request.getReviewId());
+		return ApiResponse.success();
+	}
+
+}
---
+++ src/main/java/com/cheffi/review/dto/request/DeleteBookmarkRequest.java
@@ -0,0 +1,18 @@
+package com.cheffi.review.dto.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Getter;
+
+@Getter
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class DeleteBookmarkRequest {
+	@Parameter(description = "북마크를 취소할 리뷰의 ID")
+	@NotNull
+	@PositiveOrZero
+	private Long reviewId;
+}
