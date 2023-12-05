d327a275d142c12bcc8613a905092bc283f0a779
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-12-05 22:30:39 +0900

    Feat: delegate making description logic to `CheffiCoinReasonService`

--- src/main/java/com/cheffi/avatar/service/CheffiCoinService.java
+++ src/main/java/com/cheffi/avatar/service/CheffiCoinService.java
@@ -1,6 +1,6 @@
 package com.cheffi.avatar.service;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,24 +17,25 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 public class CheffiCoinService {
 
-	private static final int CHEFFI_COIN_FOR_REVIEW = -1;
-	private static final int CHEFFI_COIN_FOR_LOGIN = 5;
+	private static final int CHEFFI_COIN_FOR_REVIEW_UNLOCK = -1;
+	private static final int CHEFFI_COIN_FOR_DAILY_REWARD = 5;
 	private final CheffiCoinRepository cheffiCoinRepository;
+	private final CheffiCoinReasonService cheffiCoinReasonService;
 	private final AvatarService avatarService;
 	private final ReviewService reviewService;
 
 	@Transactional
 	public void useCheffiCoinForUnlock(Long avatarId, Long reviewId) {
 		Review review = reviewService.getById(reviewId);
-		CheffiCoin cheffiCoin = CheffiCoin.of(avatarService.getById(avatarId), CHEFFI_COIN_FOR_REVIEW,
-			"'" + review.getTitle() + "' 게시글을 조회하는데 사용했습니다.");
+		CheffiCoin cheffiCoin = CheffiCoin.of(avatarService.getById(avatarId), CHEFFI_COIN_FOR_REVIEW_UNLOCK,
+			cheffiCoinReasonService.getReasonForReviewUnlock(review.getId()));
 		cheffiCoinRepository.save(cheffiCoin);
 	}
 
 	@Transactional
 	public void earnCheffiCoinForLogin(Long avatarId) {
 		CheffiCoin cheffiCoin = CheffiCoin.of(avatarService.getById(avatarId)
-			, CHEFFI_COIN_FOR_LOGIN, "매일 접속 쉐피 코인 획득! 날짜: " + LocalDate.now());
+			, CHEFFI_COIN_FOR_DAILY_REWARD, cheffiCoinReasonService.getReasonForDailyReward(LocalDateTime.now()));
 		cheffiCoinRepository.save(cheffiCoin);
 	}
 }
