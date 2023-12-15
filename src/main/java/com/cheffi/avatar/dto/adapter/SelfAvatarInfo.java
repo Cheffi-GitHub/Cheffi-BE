63678ea135c81025c3122f28ab22ab09500eaba2
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-12-15 16:01:45 +0900

    Feat: add `GET /my-page` API

--- src/main/java/com/cheffi/avatar/dto/adapter/SelfAvatarInfo.java
+++ src/main/java/com/cheffi/avatar/dto/adapter/SelfAvatarInfo.java
@@ -11,38 +11,49 @@ import lombok.Builder;
 @Builder
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record SelfAvatarInfo(
-
 	@Schema(description = "아바타 식별자 (아바타 = 유저 개념)")
-	Long avatarId,
+	Long id,
 	@Schema(description = "유저 닉네임")
 	String nickname,
 	@Schema(description = "유저 소개말")
 	String introduction,
 	@Schema(description = "쉐피 코인")
-	int cheffiCoinCnt,
+	int cheffiCoin,
 	@Schema(description = "포인트")
-	int pointCnt,
+	int point,
+	@Schema(description = "팔로워 수")
+	int follower,
+	@Schema(description = "팔로잉 수")
+	int following,
+	@Schema(description = "게시물 수")
+	int post,
 	@Schema(description = "프로필 사진 URL")
 	String photoUrl
 ) {
 	public static SelfAvatarInfo of(Avatar avatar, ProfilePhoto photo) {
 		return SelfAvatarInfo.builder()
-			.avatarId(avatar.getId())
+			.id(avatar.getId())
 			.nickname(avatar.getNickname())
 			.introduction(avatar.getIntroduction())
-			.cheffiCoinCnt(avatar.getCheffiCoinCnt())
-			.pointCnt(avatar.getPointCnt())
+			.cheffiCoin(avatar.getCheffiCoinCnt())
+			.point(avatar.getPointCnt())
+			.follower(avatar.getFollowerCnt())
+			.following(avatar.getFollowingCnt())
+			.post(avatar.getPostCnt())
 			.photoUrl(photo != null ? photo.getUrl() : null)
 			.build();
 	}
 
 	public static SelfAvatarInfo of(Avatar avatar) {
 		return SelfAvatarInfo.builder()
-			.avatarId(avatar.getId())
+			.id(avatar.getId())
 			.nickname(avatar.getNickname())
 			.introduction(avatar.getIntroduction())
-			.cheffiCoinCnt(avatar.getCheffiCoinCnt())
-			.pointCnt(avatar.getPointCnt())
+			.cheffiCoin(avatar.getCheffiCoinCnt())
+			.point(avatar.getPointCnt())
+			.follower(avatar.getFollowerCnt())
+			.following(avatar.getFollowingCnt())
+			.post(avatar.getPostCnt())
 			.build();
 	}
 
