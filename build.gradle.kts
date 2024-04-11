23958da2b9b08b550450dceee07ac440d32b4157
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2024-04-11 21:35:59 +0900

    Build: add webflux coroutine reactor

--- build.gradle.kts
+++ build.gradle.kts
@@ -96,6 +96,11 @@ dependencies {
     //FCM admin SDK
     implementation("com.google.firebase:firebase-admin:9.2.0")
 
+    //kotlin coroutine
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    testImplementation("io.projectreactor:reactor-test")
 }
 
 kapt {
