8e8687990c5045a8321473b2c756d39775601c1d
Lee Junpyo <wnsvy607@naver.com>
2024-07-22 17:13:14 +0900

    Feat: `POST /users/profile` 권한 검증 추가(USER)

--- src/main/java/com/cheffi/user/controller/UserController.java
+++ src/main/java/com/cheffi/user/controller/UserController.java
@@ -60,6 +60,7 @@ public class UserController {
 		description = "프로필 완료 등록 - 인증 필요 "
 			+ "닉네임 설정, 프로필 사진 등록, 태그 설정이 끝난 후에 반드시 1회 호출하여 완료 등록을 해야 합니다.",
 		security = {@SecurityRequirement(name = "session-token")})
+	@PreAuthorize("hasRole('USER')")
 	@PostMapping("/profile")
 	public ApiResponse<Void> completeProfileRegistration(
 		@AuthenticationPrincipal UserPrincipal principal) {
